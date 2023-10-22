b90e57b1c8af1f7cc9516e5d9fc9745ee07682a8
Gabriel Barbosa de Oliveira <gabrieloliveira00@outlook.com>
2023-10-21 23:56:01 -0300

    fixed leaked keys

--- instasong.py
+++ instasong.py
@@ -1,17 +1,20 @@
 import cohere
 import streamlit as st
-import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 co = cohere.Client("COHERE_API_KEY")
 
-client_id = "e1e49a36f77b48db9a29de85849ad181"
-client_secret = "c542454ae4a5401f841eb65a0c696347"
-
 sp = spotipy.Spotify(
     auth_manager=SpotifyClientCredentials(
-        client_id=client_id, client_secret=client_secret
+        client_id=CLIENT_ID, client_secret=CLIENT_SECRET
     )
 )
 
